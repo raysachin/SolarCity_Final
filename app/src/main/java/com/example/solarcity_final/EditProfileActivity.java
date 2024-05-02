dd4f40ef8fc2f6a8d3bf8e92b08b4991b6162372
raysachin <raysachin2002@gmail.com>
2024-05-03 04:29:23 +0530

    about-notification

--- app/src/main/java/com/example/solarcity_final/EditProfileActivity.java
+++ app/src/main/java/com/example/solarcity_final/EditProfileActivity.java
@@ -2,22 +2,22 @@ package com.example.solarcity_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class EditProfileActivity extends AppCompatActivity {
 
-
     EditText editName, editEmail, editUsername, editPassword;
     Button saveButton;
-    String nameUser, emailUser, usernameUser, passwordUser;
+    String usernameUser;
     DatabaseReference reference;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,48 +31,132 @@ public class EditProfileActivity extends AppCompatActivity {
         editUsername = findViewById(R.id.editUsername);
         editPassword = findViewById(R.id.editPassword);
         saveButton = findViewById(R.id.saveButton);
+
         showData();
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveProfile();
+            }
+        });
     }
 
-    public boolean isNameChanged(){
-        if (!nameUser.equals(editName.getText().toString())){
-            reference.child(usernameUser).child("name").setValue(editName.getText().toString());
-            nameUser = editName.getText().toString();
-            return true;
-        } else {
-            return false;
+    private void saveProfile() {
+        String name = editName.getText().toString().trim();
+        String email = editEmail.getText().toString().trim();
+        String password = editPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(EditProfileActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
+            return;
         }
+
+        reference.child(usernameUser).child("name").setValue(name);
+        reference.child(usernameUser).child("email").setValue(email);
+        reference.child(usernameUser).child("password").setValue(password);
+
+        Toast.makeText(EditProfileActivity.this, "Profile updated successfully", Toast.LENGTH_SHORT).show();
+        finish();
     }
 
-    private boolean isEmailChanged() {
-        if (!emailUser.equals(editEmail.getText().toString())){
-            reference.child(usernameUser).child("email").setValue(editEmail.getText().toString());
-            emailUser = editEmail.getText().toString();
-            return true;
-        } else {
-            return false;
-        }
-    }
+    private void showData() {
+        usernameUser = getIntent().getStringExtra("username");
+        String nameUser = getIntent().getStringExtra("name");
+        String emailUser = getIntent().getStringExtra("email");
+        String passwordUser = getIntent().getStringExtra("password");
 
-    private boolean isPasswordChanged() {
-        if (!passwordUser.equals(editPassword.getText().toString())){
-            reference.child(usernameUser).child("password").setValue(editPassword.getText().toString());
-            passwordUser = editPassword.getText().toString();
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public void showData(){
-        Intent intent = getIntent();
-        nameUser = intent.getStringExtra("name");
-        emailUser = intent.getStringExtra("email");
-        usernameUser = intent.getStringExtra("username");
-        passwordUser = intent.getStringExtra("password");
         editName.setText(nameUser);
         editEmail.setText(emailUser);
         editUsername.setText(usernameUser);
         editPassword.setText(passwordUser);
     }
 }
+
+
+
+
+
+
+
+
+
+
+//package com.example.solarcity_final;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.widget.Button;
+//import android.widget.EditText;
+//
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//
+//public class EditProfileActivity extends AppCompatActivity {
+//
+//
+//    EditText editName, editEmail, editUsername, editPassword;
+//    Button saveButton;
+//    String nameUser, emailUser, usernameUser, passwordUser;
+//    DatabaseReference reference;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_edit_profile);
+//
+//        reference = FirebaseDatabase.getInstance().getReference("users");
+//
+//        editName = findViewById(R.id.editName);
+//        editEmail = findViewById(R.id.editEmail);
+//        editUsername = findViewById(R.id.editUsername);
+//        editPassword = findViewById(R.id.editPassword);
+//        saveButton = findViewById(R.id.saveButton);
+//        showData();
+//    }
+//
+//    public boolean isNameChanged(){
+//        if (!nameUser.equals(editName.getText().toString())){
+//            reference.child(usernameUser).child("name").setValue(editName.getText().toString());
+//            nameUser = editName.getText().toString();
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    private boolean isEmailChanged() {
+//        if (!emailUser.equals(editEmail.getText().toString())){
+//            reference.child(usernameUser).child("email").setValue(editEmail.getText().toString());
+//            emailUser = editEmail.getText().toString();
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    private boolean isPasswordChanged() {
+//        if (!passwordUser.equals(editPassword.getText().toString())){
+//            reference.child(usernameUser).child("password").setValue(editPassword.getText().toString());
+//            passwordUser = editPassword.getText().toString();
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    public void showData(){
+//        Intent intent = getIntent();
+//        nameUser = intent.getStringExtra("name");
+//        emailUser = intent.getStringExtra("email");
+//        usernameUser = intent.getStringExtra("username");
+//        passwordUser = intent.getStringExtra("password");
+//        editName.setText(nameUser);
+//        editEmail.setText(emailUser);
+//        editUsername.setText(usernameUser);
+//        editPassword.setText(passwordUser);
+//    }
+//}
--- app/src/main/java/com/example/solarcity_final/HomeFragment.java
+++ app/src/main/java/com/example/solarcity_final/HomeFragment.java
@@ -1,13 +1,7 @@
 package com.example.solarcity_final;
 
-import static android.service.controls.ControlsProviderService.TAG;
-
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +9,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,7 +24,7 @@ public class HomeFragment extends Fragment {
 
     TextView profileName, profileEmail, profileUsername, profilePassword;
     TextView titleName, titleUsername;
-    Button editProfile;
+    Button editProfile, logoutButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,15 +37,16 @@ public class HomeFragment extends Fragment {
         profileEmail = view.findViewById(R.id.profileEmail);
         profileUsername = view.findViewById(R.id.profileUsername);
         profilePassword = view.findViewById(R.id.profilePassword);
+        profilePassword.setVisibility(View.GONE);
         titleName = view.findViewById(R.id.titleName);
         titleUsername = view.findViewById(R.id.titleUsername);
 
-        // Initialize Button
+        // Initialize Buttons
         editProfile = view.findViewById(R.id.editButton);
+        logoutButton = view.findViewById(R.id.logoutButton);
 
         // Read data from Firebase Database
-
-         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users");
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -80,7 +79,7 @@ public class HomeFragment extends Fragment {
             @Override
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+                Log.w("TAG", "Failed to read value.", error.toException());
             }
         });
 
@@ -89,6 +88,20 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 passUserData();
+            }
+        });
+
+        // Button click listener to handle logout
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Sign out the user
+                FirebaseAuth.getInstance().signOut();
+
+                // Navigate back to the login page
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
         });
 
@@ -126,6 +139,146 @@ public class HomeFragment extends Fragment {
         });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+//package com.example.solarcity_final;
+//
+//import static android.service.controls.ControlsProviderService.TAG;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//
+//import androidx.annotation.NonNull;
+//import androidx.fragment.app.Fragment;
+//
+//import android.util.Log;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.Button;
+//import android.widget.TextView;
+//
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.Query;
+//import com.google.firebase.database.ValueEventListener;
+//
+//public class HomeFragment extends Fragment {
+//
+//    TextView profileName, profileEmail, profileUsername, profilePassword;
+//    TextView titleName, titleUsername;
+//    Button editProfile;
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//
+//        View view = inflater.inflate(R.layout.fragment_home, container, false);
+//
+//        // Initialize TextViews
+//        profileName = view.findViewById(R.id.profileName);
+//        profileEmail = view.findViewById(R.id.profileEmail);
+//        profileUsername = view.findViewById(R.id.profileUsername);
+//        profilePassword = view.findViewById(R.id.profilePassword);
+//        profilePassword.setVisibility(View.GONE);
+//        titleName = view.findViewById(R.id.titleName);
+//        titleUsername = view.findViewById(R.id.titleUsername);
+//
+//        // Initialize Button
+//        editProfile = view.findViewById(R.id.editButton);
+//
+//        // Read data from Firebase Database
+//
+//         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users");
+//
+//        databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // Clear previous data to prevent duplication
+//                titleName.setText("");
+//                titleUsername.setText("");
+//                profileName.setText("");
+//                profileEmail.setText("");
+//                profileUsername.setText("");
+//                profilePassword.setText("");
+//
+//                // Iterate through each user data
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                    String name = snapshot.child("name").getValue(String.class);
+//                    String email = snapshot.child("email").getValue(String.class);
+//                    String username = snapshot.child("username").getValue(String.class);
+//                    String password = snapshot.child("password").getValue(String.class);
+//
+//                    // Update UI with the latest data
+//                    titleName.setText(name);
+//                    titleUsername.setText(username);
+//                    profileName.setText(name);
+//                    profileEmail.setText(email);
+//                    profileUsername.setText(username);
+//                    profilePassword.setText(password);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
+//
+//        // Button click listener to pass user data to EditProfileActivity
+//        editProfile.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                passUserData();
+//            }
+//        });
+//
+//        return view;
+//    }
+//
+//    private void passUserData() {
+//        String userUsername = profileUsername.getText().toString().trim();
+//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+//        Query checkUserDatabase = reference.orderByChild("username").equalTo(userUsername);
+//
+//        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                if (snapshot.exists()) {
+//                    String nameFromDB = snapshot.child(userUsername).child("name").getValue(String.class);
+//                    String emailFromDB = snapshot.child(userUsername).child("email").getValue(String.class);
+//                    String usernameFromDB = snapshot.child(userUsername).child("username").getValue(String.class);
+//                    String passwordFromDB = snapshot.child(userUsername).child("password").getValue(String.class);
+//
+//                    // Start EditProfileActivity and pass user data
+//                    Intent intent = new Intent(getContext(), EditProfileActivity.class);
+//                    intent.putExtra("name", nameFromDB);
+//                    intent.putExtra("email", emailFromDB);
+//                    intent.putExtra("username", usernameFromDB);
+//                    intent.putExtra("password", passwordFromDB);
+//                    startActivity(intent);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//                // Handle onCancelled event
+//            }
+//        });
+//    }
+//}
 
 
 
